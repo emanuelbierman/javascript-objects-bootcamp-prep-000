4a140162444d4143e3cea95411d3a4f0c93f71e4
Emanuel Bierman <emanuel.bierman@gmail.com>
2017-11-06 15:39:33 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,15 @@
+
+var playlist = {
+  jamesBrown: 'Red Hot',
+  incubus: 'Morning View'
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName = 'songTitle'
+  return playlist
+}
+
+function removeFrom Playlist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist
+}
