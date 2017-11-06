be1825e87e0c8031d01c8966179874b190e4f71b
Emanuel Bierman <emanuel.bierman@gmail.com>
2017-11-06 15:46:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,3 +8,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist["Phil Ochs"] = "Here's to the State of Mississippi"
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Chon
+  return playlist
+}
