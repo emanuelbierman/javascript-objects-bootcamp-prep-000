f98748ccbc276a2dc539d97648561da620b5371a
Emanuel Bierman <emanuel.bierman@gmail.com>
2017-11-06 15:41:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,15 +1,5 @@
 
 var playlist = {
-  jamesBrown: 'Red Hot',
-  incubus: 'Morning View'
-}
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = 'songTitle'
-  return playlist
-}
-
-function removeFrom Playlist(playlist, artistName) {
-  delete playlist.artistName
-  return playlist
+  Chon: "Bubble Dreams",
+  MJ: "Thriller"
 }
